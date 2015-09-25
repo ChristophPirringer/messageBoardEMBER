e8e09563deb1cd6920e00305bd043e1eef833b9d
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-09-25 10:36:52 -0700

    add functionality for answers to questions

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveAnswer() {
+      var params = {
+        username: this.get('username'),
+        date: this.get('date'),
+        content: this.get('content'),
+        question: this.get('question')
+      };
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
