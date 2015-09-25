f3766866f1b22b9b8040c68400677afb6ec66d0b
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-09-25 11:20:03 -0700

    add functionality to display answers to each quesiton, edit and delete them

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
 
   actions: {
     update(question, params) {
-      this.sendAction('update', queston, params);
+      this.sendAction('update', question, params);
     },
     delete(question) {
       if(confirm('Are you sure you want to delete this piece of art?')){
