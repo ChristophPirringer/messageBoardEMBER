52b5b9930dd0040df03443500778fabcaf1a6c10
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-09-25 10:49:19 -0700

    add functionality to delete or edit questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      blogposts: this.store.findAll('question'),
+      questions: this.store.findAll('question'),
       comments: this.store.findAll('answer')
     });
   },
 
   actions: {
     save(params){
-      var newBlogpost = this.store.createRecord('question', params);
+      var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
     },
@@ -18,15 +18,15 @@ export default Ember.Route.extend({
     update(question, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
-          blogpost.set(key, params[key]);
+          question.set(key, params[key]);
         }
       });
       question.save();
       this.transitionTo('index');
     },
 
-    destroyQuestion(blogpost) {
-      blogpost.get('answers').forEach(function(answer){
+    destroyQuestion(question) {
+      question.get('answers').forEach(function(answer){
         answer.destroyRecord();
       });
       question.destroyRecord();
